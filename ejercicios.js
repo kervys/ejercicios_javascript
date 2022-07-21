4939cbdbd3e3b6701b753b47e85e1b8641770e2b
unknown <kervysjulian@gmail.com>
2022-07-20 21:25:09 -0400

    Primeros Ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,11 @@
+console.log('Ejercicios de pares e impares');
+
+var num1 = 6;
+var num2 = 2;
+if(num1 <= 0){console.log('Debe corregir valor introducido')} else if(num1 => 1){
+  resultado = num1 % num2;
+  if(resultado === 0){console.log("Par")}
+  else {console.log("Impar")}
+}
+
+console.log('Ejercicio de ');
