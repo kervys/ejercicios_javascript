3af539cefc63e52bc0d00c549d3a1dfb4f311b8d
unknown <kervysjulian@gmail.com>
2022-10-21 22:41:47 -0400

    Ejercicios

--- ejercicios.js
+++ ejercicios.js
@@ -1,11 +1,38 @@
 console.log('Ejercicios de pares e impares');
 
-var num1 = 6;
+var num1 = 7; ///solo midificar este numero para validar si es par o no
 var num2 = 2;
 if(num1 <= 0){console.log('Debe corregir valor introducido')} else if(num1 => 1){
   resultado = num1 % num2;
-  if(resultado === 0){console.log("Par")}
-  else {console.log("Impar")}
+  if(resultado === 0){console.log("El numero "+ num1 +" es Par")}
+  else {console.log("El numero " + num1 + " es Impar")}
 }
+///
+console.log('Ejercicios de pares e impares');
+for(contador=0; contador <= 20; contador++){
+  if (contador % 2 == 0){
 
+  }
+  cuenta++;
+  debugger;
+}
+console.log("Hay " + cuenta + " numeros impares");
+/////
+
+console.log('Ejercicio de Ventas'); 
+///Ventas de productos
+"use strict"
+///colocar cantidad del producto
+var producto = 0;
+
+while (producto > 0){
+  console.log('Producto Vendido');
+  producto--;
+ console.log('Inventario ' + producto );
+}
+ console.log('Inventario ' + producto );
+
+
+///
 console.log('Ejercicio de ');
+///
